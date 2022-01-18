f5d9046a16079a8b17e7a9e4c41659774e7993d9
Matt Rangel <mattrangel@gmail.com>
2022-01-17 19:04:01 -0800

    Encode functions for cities and cookie counts for each hour in each city

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-
-
 let hours = ['6am', '7am', '8am','9am', '10am', '11am','12pm', '1pm', '2am','3pm','4pm', '5pm', '6pm','7pm'];
+
+//1st step: Grab your window into the DOM, using the getElementById method. See line #77 in code
 
 let cookieSales = document.getElementById('cookie-sales');
 
@@ -10,46 +10,120 @@ console.log(cookieSales);
 
 let seattle = {
   city: 'Seattle',
-  cust: null,
+  cust: [],
   avgCookie: 6.3,
+  cookieArr: [],
+  minCust: 23,
+  maxCust: 65,
+  totalCookies: 0,
   getCust: function () {
-    this.cust = `${randomCust(23,65)} customers`;
+    for(let i = 0; i < hours.length; i++){
+      this.cust.push(randomCust(this.minCust,this.maxCust));
+    }
+  },
+  calcCookie: function () {
+    this.getCust();
+    for(let i = 0; i < hours.length; i++){
+      let cookieBatch = Math.ceil(this.avgCookie * this.cust[i]);
+      this.cookieArr.push(cookieBatch);
+      this.totalCookies = this.totalCookies + cookieBatch;
+    }
   }
 };
+// seattle.getCust();
+// seattle.calcCookie();
+
+console.log(seattle);
 
 let tokyo = {
   city: 'Tokyo',
-  cust: null,
-  avgCookie: 6.3,
+  cust: [],
+  avgCookie: 1.2,
+  cookieArr: [],
+  minCust: 3,
+  maxCust: 24,
+  totalCookies: 0,
   getCust: function () {
-    this.cust = `${randomCust(23,65)} customers`;
+    for(let i = 0; i < hours.length; i++){
+      this.cust.push(randomCust(this.minCust,this.maxCust));
+    }
+  },
+  calcCookie: function () {
+    this.getCust();
+    for(let i = 0; i < hours.length; i++){
+      let cookieBatch = Math.ceil(this.avgCookie * this.cust[i]);
+      this.cookieArr.push(cookieBatch);
+      this.totalCookies = this.totalCookies + cookieBatch;
+    }
   }
 };
 
 let dubai = {
   city: 'Dubai',
-  cust: null,
-  avgCookie: 6.3,
+  cust: [],
+  avgCookie: 3.7,
+  cookieArr: [],
+  minCust: 11,
+  maxCust: 38,
+  totalCookies: 0,
   getCust: function () {
-    this.cust = `${randomCust(23,65)} customers`;
+    for(let i = 0; i < hours.length; i++){
+      this.cust.push(randomCust(this.minCust,this.maxCust));
+    }
+  },
+  calcCookie: function () {
+    this.getCust();
+    for(let i = 0; i < hours.length; i++){
+      let cookieBatch = Math.ceil(this.avgCookie * this.cust[i]);
+      this.cookieArr.push(cookieBatch);
+      this.totalCookies = this.totalCookies + cookieBatch;
+    }
   }
 };
 
 let paris = {
   city: 'Paris',
-  cust: null,
-  avgCookie: 6.3,
+  cust: [],
+  avgCookie: 2.3,
+  cookieArr: [],
+  minCust: 20,
+  maxCust: 38,
+  totalCookies: 0,
   getCust: function () {
-    this.cust = `${randomCust(23,65)} customers`;
+    for(let i = 0; i < hours.length; i++){
+      this.cust.push(randomCust(this.minCust,this.maxCust));
+    }
+  },
+  calcCookie: function () {
+    this.getCust();
+    for(let i = 0; i < hours.length; i++){
+      let cookieBatch = Math.ceil(this.avgCookie * this.cust[i]);
+      this.cookieArr.push(cookieBatch);
+      this.totalCookies = this.totalCookies + cookieBatch;
+    }
   }
 };
 
 let lima = {
   city: 'Lima',
-  cust: null,
-  avgCookie: 6.3,
+  cust: [],
+  avgCookie: 4.6,
+  cookieArr: [],
+  minCust: 2,
+  maxCust: 16,
+  totalCookies: 0,
   getCust: function () {
-    this.cust = `${randomCust(23,65)} customers`;
+    for(let i = 0; i < hours.length; i++){
+      this.cust.push(randomCust(this.minCust,this.maxCust));
+    }
+  },
+  calcCookie: function () {
+    this.getCust();
+    for(let i = 0; i < hours.length; i++){
+      let cookieBatch = Math.ceil(this.avgCookie * this.cust[i]);
+      this.cookieArr.push(cookieBatch);
+      this.totalCookies = this.totalCookies + cookieBatch;
+    }
   }
 };
 
@@ -60,19 +134,12 @@ function randomCust(min, max) {
 
 const cities = [seattle, tokyo, dubai, paris, lima];
 
-function getAllCustomers(){
-  for(let i = 0; i < cities.length; i++) {
-    let currentCity = cities[i];
-    currentCity.getCust();
-  }
-}
-
-getAllCustomers();
-
+// getAllCustomers();
+console.log(cities);
 // ************** DOM Manipulation ***********************
 
 // STEPS:
-// 1st step: Grab your window into the DOM, using the getElementById method. See line #19 in code
+// 1st step: Grab your window into the DOM, using the getElementById method. See line #9 in code
 // 2nd step: Create the element using the createElement method - takes in a string of the element type you want to create ('p') <- creates a p tag element
 // 3rd step: Give that element context (if you want or need to)
 // 4th step: append(add) to the DOM <-- parentElem.appendChild(childElem)
@@ -87,3 +154,45 @@ getAllCustomers();
 //        </ul>
 //        <img>
 //      </article> -->
+
+function renderCookies(store) {
+  store.calcCookie();
+  // create an article element, it doesn't need context but append it to our DOM window, cookieSales
+  const articleElem = document.createElement('article');
+  cookieSales.appendChild(articleElem);
+
+  //create h2 element, give it context
+  // and append it to the article element
+  const h2Elem = document.createElement('h2');
+  h2Elem.textContent = store.city;
+  articleElem.appendChild(h2Elem);
+
+  // const pElem = document.createElement('p');
+  // pElem.textContent = '${cookie.name} is selling ${cookies.total}';
+  // // to do: create a function with total cookies??
+  // articleElem.appendChild(pElem);
+
+  const ulElem = document.createElement('ul');
+  articleElem.appendChild(ulElem);
+
+  // loop through the hours array to create lis
+  for(let i = 0; i < hours.length; i++){
+    // 6am: 16 cookies
+
+    const liElem = document.createElement('li');
+    liElem.textContent = `${hours[i]}: ${store.cookieArr[i]} cookies`;
+    ulElem.appendChild(liElem);
+  }
+  const liElem = document.createElement('li');
+  liElem.textContent = `Total: ${store.totalCookies} cookies`;
+  ulElem.appendChild(liElem);
+}
+// for loop through cities
+function renderAllCities() {
+  for (let i = 0; i < cities.length; i++){
+    let currentCity = cities[i];
+    renderCookies(currentCity);
+  }
+}
+
+renderAllCities();
